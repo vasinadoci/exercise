de21c775794e66405324c07ed84940e680335d79
vasinadoci <87311796+vasinadoci@users.noreply.github.com>
2022-01-19 15:26:01 +0300

    Add files via upload

---
+++ java/exercice-min.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+   $('.header__menu-burger').click(function () {
+      $('.header__menu-burger').toggleClass('open-menu');
+      $('.header__menu').toggleClass('open-menu');
+   });
+});
